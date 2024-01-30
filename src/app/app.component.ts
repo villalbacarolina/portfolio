39ed09d92402e1ecaf49f177d95b3ea2b0683773
Carolina Villalba <carolinaangelesvillalba@email.com>
2024-01-30 11:55:33 -0300

    experience finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EducationComponent } from "./education/education.component";
+import { ExperienceComponent } from "./experience/experience.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, EducationComponent]
+    imports: [RouterOutlet, EducationComponent, ExperienceComponent]
 })
 export class AppComponent {
   title = 'front-end';
