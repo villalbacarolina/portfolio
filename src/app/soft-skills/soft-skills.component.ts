494dc9788b7d17149e9bb353dea2d39e1d1c606d
Carolina Villalba <carolinaangelesvillalba@email.com>
2024-02-05 02:46:43 -0300

    structure finished

---
+++ src/app/soft-skills/soft-skills.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soft-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './soft-skills.component.html',
+  styleUrl: './soft-skills.component.css'
+})
+export class SoftSkillsComponent {
+
+}
