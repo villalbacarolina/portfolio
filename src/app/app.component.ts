ad18f73f989e292cf0437d8c327ddf4a19f4b797
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-03-10 10:41:43 -0300

    style: all titles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { TechnologiesComponent } from "./technologies/technologies.component";
 import { SoftSkillsComponent } from "./soft-skills/soft-skills.component";
 import { HeaderComponent } from "./header/header.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, EducationComponent, ExperienceComponent, TechnologiesComponent, SoftSkillsComponent, HeaderComponent, AboutMeComponent]
+    imports: [RouterOutlet, EducationComponent, ExperienceComponent, TechnologiesComponent, SoftSkillsComponent, HeaderComponent, AboutMeComponent, ContactComponent, ProjectsComponent]
 })
 export class AppComponent {
   title = 'front-end';
